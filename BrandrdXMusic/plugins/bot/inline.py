a6dd5df1a3035fe2d8d05dc2551d3bd593187c59
vkm832 <bot627668@gmail.com>
2025-09-18 08:42:34 +0530

    Update inline.py

--- BrandrdXMusic/plugins/bot/inline.py
+++ BrandrdXMusic/plugins/bot/inline.py
@@ -42,16 +42,23 @@ async def inline_query_handler(client, query):
                     ],
                 ]
             )
+
+            # 🔥 Stylish Duration Line Added Here
+            duration_line = f"════════◉════════\n{duration}"
+
             searched_text = f"""
 ❄ <b>ᴛɪᴛʟᴇ :</b> <a href={link}>{title}</a>
 
 ⏳ <b>ᴅᴜʀᴀᴛɪᴏɴ :</b> {duration} ᴍɪɴᴜᴛᴇs
+{duration_line}
+
 👀 <b>ᴠɪᴇᴡs :</b> <code>{views}</code>
 🎥 <b>ᴄʜᴀɴɴᴇʟ :</b> <a href={channellink}>{channel}</a>
 ⏰ <b>ᴘᴜʙʟɪsʜᴇᴅ ᴏɴ :</b> {published}
 
 
 <u><b>➻ ɪɴʟɪɴᴇ sᴇᴀʀᴄʜ ᴍᴏᴅᴇ ʙʏ {app.name}</b></u>"""
+
             answers.append(
                 InlineQueryResultPhoto(
                     photo_url=thumbnail,
